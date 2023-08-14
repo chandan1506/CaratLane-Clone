a21f9f62356ae70e15a2f37000abf72b15d1cfa2
chandan kumar <ck893607@gmail.com>
2023-08-14 10:58:00 +0530

    updated logic for rendering data

--- frontend/scripts/solitaires.js
+++ frontend/scripts/solitaires.js
@@ -32,7 +32,7 @@ function renderdata(data) {
       <div id="card">
                     <div id="cardtop">
                         <div>
-                            <p id="seller">${element.seller}</p>
+                            <p id="seller">${element.seller || ""}</p>
                         </div>
                         <div >
                             <button id="wishlist" > 
